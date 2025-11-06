8cbb090be4d6dd1d81d365a9b98c6e2283319f41
lvanias <lvanias@hawk.illinoistech.edu>
2025-11-06 14:56:57 -0600

    Update script.js

--- lab7/js/script.js
+++ lab7/js/script.js
@@ -2,6 +2,7 @@ const alertMe = document.getElementById("entrybutton");
 const input = document.getElementById("entryinput");
 const output = document.getElementById("textoutput");
 var value;
+
 function init(){
   alertMe.addEventListener("click", function() {
     value = input.value;
@@ -11,3 +12,4 @@ function init(){
 }
 
 window.addEventListener('load', init);
+
