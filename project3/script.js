d9e1c4438ae42f5bb2e91a3f0b06a047042f0d99
lvanias <lvanias@hawk.illinoistech.edu>
2025-11-25 23:40:43 -0600

    Add files via upload

---
+++ project3/script.js
@@ -0,0 +1,18 @@
+function askQuestion() {
+    alert('If you have questions, contact me at:\nlvanias@hawk.illinoistech.edu');
+  };
+
+  function reloadMap() {
+    map = new google.maps.Map(document.getElementById("googleMap"), {
+      center: { lat: 51.508742, lng: -0.120850},
+      zoom: 5
+    });
+  }
+
+function expandMap() {
+ document.getElementById("googleMap").classList.toggle("expanded");
+}
+
+  document.getElementById("questionButton").addEventListener("click", askQuestion);
+  document.getElementById("reloadButton").addEventListener("click", reloadMap);
+  document.getElementById("expandButton").addEventListener("click", expandMap);
